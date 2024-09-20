220825dd3ff27dd1c8c88c6e14a618b876de6b36
Merve CALISKAN <114764908+Mervecaliskann@users.noreply.github.com>
2024-09-20 17:16:05 +0300

    Update d0287e82-19f4-4f0b-a8b6-6d74d9ff6b5cHomework3_MerveCaliskan.sql

--- d0287e82-19f4-4f0b-a8b6-6d74d9ff6b5cHomework3_MerveCaliskan.sql
+++ d0287e82-19f4-4f0b-a8b6-6d74d9ff6b5cHomework3_MerveCaliskan.sql
@@ -1,20 +1,20 @@
-WITH birlesik_tablo AS (
-    SELECT ad_date, 'Facebook Ads' AS media_source, spend, impressions, reach, clicks, leads, value
-    FROM facebook_ads_basic_daily
-    UNION ALL
-    SELECT ad_date, 'Google Ads' AS media_source, spend, impressions, reach, clicks, leads, value 
-    FROM google_ads_basic_daily
+WITH combined_table AS (
+    SELECT ad_date, 'Facebook Ads' AS media_source, spend, impressions, reach, clicks, leads, value
+    FROM facebook_ads_basic_daily
+    UNION ALL
+    SELECT ad_date, 'Google Ads' AS media_source, spend, impressions, reach, clicks, leads, value 
+    FROM google_ads_basic_daily
 )
 
-SELECT 
-    ad_date,
-    media_source,
-    SUM(spend) AS toplam_maliyet,
-    SUM(impressions) AS toplam_goruntuleme,
-    SUM(clicks) AS toplam_tiklama,
-    SUM(value) AS value,
-    SUM(clicks)::numeric /SUM(impressions)::numeric AS conversion_rate
-FROM birlesik_tablo
-WHERE clicks > 0 AND impressions > 0 AND clicks IS NOT NULL AND impressions IS NOT NULL 
+SELECT 
+    ad_date,
+    media_source,
+    SUM(spend) AS total_cost,
+    SUM(impressions) AS total_impressions,
+    SUM(clicks) AS total_clicks,
+    SUM(value) AS value,
+    SUM(clicks)::numeric / SUM(impressions)::numeric AS conversion_rate
+FROM combined_table
+WHERE clicks > 0 AND impressions > 0 AND clicks IS NOT NULL AND impressions IS NOT NULL 
 GROUP BY ad_date, media_source
 ORDER BY ad_date, media_source;
